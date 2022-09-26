29a5fe59373a02d9698383c4322a3a86ddb4cd3f
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2022-09-26 17:25:47 +0500

    Changed Base Url

--- src/Util/backendurl.ts
+++ src/Util/backendurl.ts
@@ -1,2 +1,2 @@
-// export const backendUrl = 'https://odd-puce-chinchilla.cyclic.app/';
-export const backendUrl = 'http://localhost:5000';
+export const backendUrl = 'https://odd-puce-chinchilla.cyclic.app/';
+// export const backendUrl = 'http://localhost:5000';
